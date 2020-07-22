4f3335c9702803f5707933754520919b6a0f90ad
master <shyngys99106@gmail.com>
2020-07-23 00:42:52 +0600

    first task

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+
+import togglePopUp from './modules/togglePopUp';
+
+
+
+
+togglePopUp();
+
+
---
+++ src/modules/togglePopUp.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+const togglePopup = () => {
+  console.log('12');
+};
+
+export default togglePopup;
