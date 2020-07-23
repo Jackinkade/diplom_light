4dd63b8a90387c5fbb3a2d5a30c78440a69b6fc1
master <shyngys99106@gmail.com>
2020-07-23 23:31:27 +0600

    Больше - конпка

--- src/modules/addElems.js
+++ src/modules/addElems.js
@@ -1,16 +1,20 @@
 const addElems = () => {
-	const addButton = document.querySelector('.add-sentence-btn');
-
-	addButton.style.display = 'block';
-	addButton.style.position = 'relative';
-	addButton.style.textAligin = 'center';
-	addButton.style.left = 45 + '%';
-
-	const removeButton = () => {
-		addButton.style.display = 'none';
-	};
-	addButton.addEventListener('click', removeButton);
+	const showHiddenBox = document.querySelectorAll(`.col-xs-12.col-sm-6.col-md-4`),
+		buttonBolshe = document.querySelector('.add-sentence-btn');
 
 
-};//нодо убрать класс hidden & visible-sb-block
+	buttonBolshe.addEventListener('click', event => {
+		event.preventDefault();
+		showHiddenBox.forEach(elem => {
+			if (elem.matches('.visible-sm-block') || elem.matches('.hidden')) {
+				elem.classList.remove('visible-sm-block', 'hidden');
+				buttonBolshe.style.cssText = `transform: scale(0)`;
+			}
+
+		});
+
+	});
+
+};
+//нодо убрать класс hidden & visible-sb-block
 export default addElems;
