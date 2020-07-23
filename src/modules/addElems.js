f39fa6fb90b0dd7d47d99d4c236857d231022f11
master <shyngys99106@gmail.com>
2020-07-23 22:20:43 +0600

    novalidate

--- src/modules/addElems.js
+++ src/modules/addElems.js
@@ -1,6 +1,16 @@
 const addElems = () => {
 	const addButton = document.querySelector('.add-sentence-btn');
 
-	console.log(addButton);
-};
+	addButton.style.display = 'block';
+	addButton.style.position = 'relative';
+	addButton.style.textAligin = 'center';
+	addButton.style.left = 45 + '%';
+
+	const removeButton = () => {
+		addButton.style.display = 'none';
+	};
+	addButton.addEventListener('click', removeButton);
+
+
+};//нодо убрать класс hidden & visible-sb-block
 export default addElems;
