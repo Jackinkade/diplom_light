b237f50c6c75a88b4e678c823b2f971e8c263fa5
master <shyngys99106@gmail.com>
2020-07-24 20:46:27 +0600

    less code

--- src/modules/simleAccardion.js
+++ src/modules/simleAccardion.js
@@ -8,40 +8,28 @@ const simleAcc = () => {
 	btnAccordion.forEach(elem => {
 		elem.addEventListener('click', event => {
 			event.preventDefault();
-			//One
-			if (elem.closest('#headingOne-two')) {
-				if (collapseOneId.style.display === 'none') {
-					collapseOneId.style.display = 'block';
-					collapseTwoId.style.display = 'none';
-					collapseThreeId.style.display = 'none';
+			const foo = (one, two, tree) => {
+				if (one.style.display === 'block') { //collapseThreeId
+					one.style.display = 'block';//collapseThreeId
 
 				} else {
-					collapseOneId.style.display = 'block';
+					tree.style.display = 'none';//collapseOneId
+					two.style.display = 'none';//collapseTwoId
+					one.style.display = 'block';//collapseThreeId
 				}
+			};
+			//One
+			if (elem.closest('#headingOne-two')) {
+				foo(collapseOneId, collapseTwoId, collapseThreeId);
 			}
 			//Two
 			if (elem.closest('#headingTwo-two') || elem.matches('.btnOne')) {
-
-				if (collapseTwoId.style.display === 'block') {
-					collapseTwoId.style.display = 'block';
-
-				} else {
-					collapseOneId.style.display = 'none';
-					collapseTwoId.style.display = 'block';
-					collapseThreeId.style.display = 'none';
-				}
+				foo(collapseTwoId, collapseOneId, collapseThreeId);
 			}
 			//Three
 			if (elem.closest('#headingThree-two') || elem.matches('.btnTwo')) {
 
-				if (collapseThreeId.style.display === 'block') {
-					collapseThreeId.style.display = 'block';
-
-				} else {
-					collapseOneId.style.display = 'none';
-					collapseTwoId.style.display = 'none';
-					collapseThreeId.style.display = 'block';
-				}
+				foo(collapseThreeId, collapseTwoId, collapseOneId);
 			}
 
 		});
